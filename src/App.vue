96ece66b7620880fbf69b407a34c1433c5a4d682
13936252133 <ls57765867@126.com>
2023-03-31 17:41:02 +0800

    添加tailwindcss

--- src/App.vue
+++ src/App.vue
@@ -3,8 +3,8 @@
   <div>双倍年龄是：{{ doubleAge }}</div>
   <el-button @click="editName">换个名字</el-button>
   <el-button @click="editAge">年龄+1</el-button>
-  <div>
-    <router-view style="margin-top: 120px"></router-view>
+  <div class="text-green-800 mt-10">
+    <router-view></router-view>
   </div>
 </template>
 
