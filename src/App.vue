fbd85a4ceb2d5d692fe65030063e620b17ecb970
13936252133 <ls57765867@126.com>
2023-03-31 17:07:08 +0800

    vue3 ts eslint router pinia

---
+++ src/App.vue
@@ -0,0 +1,24 @@
+<template>
+  <div>{{ name }}今年{{ age }}了</div>
+  <div>双倍年龄是：{{ doubleAge }}</div>
+  <el-button @click="editName">换个名字</el-button>
+  <el-button @click="editAge">年龄+1</el-button>
+  <div>
+    <router-view style="margin-top: 120px"></router-view>
+  </div>
+</template>
+
+<script setup lang="ts">
+import { useBaseStore } from './store'
+import { storeToRefs } from 'pinia'
+
+const { name, age, doubleAge } = storeToRefs(useBaseStore())
+const store = useBaseStore()
+
+const editName = () => {
+  store.setName()
+}
+const editAge = () => {
+  age.value++
+}
+</script>
